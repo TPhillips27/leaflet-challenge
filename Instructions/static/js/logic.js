faf2011ec1d2811c80c35f45da8b7e4bba583340
TPhillips27 <69609977+TPhillips27@users.noreply.github.com>
2021-02-05 15:47:10 -0500

    update

--- Instructions/static/js/logic.js
+++ Instructions/static/js/logic.js
@@ -15,7 +15,8 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   }).addTo(myMap);
 
   // Variable for endpoint API data
-var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+
 
 //Write get request of API data from above url
 //add in a bit more here to read
@@ -23,13 +24,41 @@ var quakeURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_we
 d3.json(quakeURL, function (json) {
   // features refers to the section in the url where magnitude (mag) is placed
     var data = json.features;
-    console.log(data);
+    //console.log(data);
+
+      //how to change color for each location 
+    function circleFillColor(mag) {
+      switch (true) {
+        case mag > 9 :
+          return 'blue';
+        case mag > 8:
+          return 'red';
+        case mag > 7:
+            return 'yellow';
+        case mag > 6:
+            return 'purple';
+        case mag > 5:
+            return 'green';
+        case mag > 4:
+            return 'orange';
+        case mag > 3:
+            return 'brown';
+        case mag > 2:
+            return 'pink';
+        case mag > 1:
+            return 'black';
+        default: 
+          return 'teal'
+      };
+    }
+
     var geoJsonLayer =   L.geoJson(data, {
       // Changing to circles 
       pointToLayer: function(feature, latlng) {
         return new L.CircleMarker(latlng, {
           radius: (feature.properties.mag)*5,
-          color: 'red'
+          color: circleFillColor(feature.properties.mag)
+          //fillOpacity: 1
         });
       },
       // Called on each feature
@@ -37,65 +66,26 @@ d3.json(quakeURL, function (json) {
         layer.bindPopup("<h1>" + feature.geometry.coordinates + "</h1> <hr> <h2>" + feature.properties.mag + "</h2>");
       }
     }).addTo(myMap);
+
+
+       //add legend
+       var legend = L.control({position: 'bottomright'});
+
+       legend.onAdd = function (map) {
+       
+           var div = L.DomUtil.create('div', 'info legend'),
+               magnitude = [1, 2, 3, 4, 5, 6, 7, 8, 9],
+               labels = [];
+       
+           // loop through our density intervals and generate a label with a colored square for each interval
+           for (var i = 0; i < magnitude.length; i++) {
+               div.innerHTML +=
+                   '<i style="background:' + circleFillColor(magnitude[i] + 1) + '"></i> ' +
+                   magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
+           }
+       
+           return div;
+       };
+       
+       legend.addTo(map);
 });
-  
-  //how to change color for each location 
-  // function circleFillColor(mag) {
-  //   switch (true) {
-  //     case 'mag > 1 ':
-  //       return 'blue';
-  //     case 'mag > 2':
-  //       return 'red';
-  //     case 'mag > 3 ':
-  //         return 'yellow';
-  //     case 'mag > 4':
-  //         return 'purple';
-  //     case 'mag > 5 ':
-  //         return 'green';
-  //     case 'mag > 6':
-  //         return 'orange';
-  //     case 'mag > 7 ':
-  //         return 'brown';
-  //     case 'mag > 8':
-  //         return 'pink';
-  //     case 'mag > 9':
-  //         return 'black';
-  //     default: 
-  //       return 'white'
-  //   };
-  // }
-
-//   function style(feature) {
-//     return {
-//         fillColor: getColor(feature.properties.mag),
-//         weight: 2,
-//         opacity: 1,
-//         color: 'white',
-//         dashArray: '3',
-//         fillOpacity: 0.7
-//     };
-// }
-
-// .addTo(mymap);
-
-
-    //add legend
-  var legend = L.control({position: 'bottomright'});
-
-  legend.onAdd = function () {
-    
-  var div = L.DomUtil.create('div', 'info legend'),
-    magnitude = [0, 1, 2, 3, 4, 5, 6, 7 , 8, 9];
-            //labels = [];
-    
-        // loop through our density intervals and generate a label with a colored square for each interval
-  for (var i = 0; i < magnitude.length; i++) {
-    div.innerHTML +=
-    '<i style="background:' + getColor(magnitude[i] + 1) + '"></i> ' +
-    magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
-  }
-    
-      return div;
-    };
-    
-    legend.addTo(map);
